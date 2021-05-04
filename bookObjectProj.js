d15d12780504156d5edbbc72311f38a05f6c3274
jl232367 <48839916+jl232367@users.noreply.github.com>
2021-05-03 20:47:13 -0400

    refactored to include factory function and module syntax to reduce window namespace

--- bookObjectProj.js
+++ bookObjectProj.js
@@ -12,25 +12,20 @@ const pBook = (title, author, pages, read) => {
             read = "True";
         }
     };
-    return {bookInfo}
+    return {bookInfo, readToggle}
 }
 
 const hobbit = pBook ("The Hobbit", "J.R.R Tolkien", 295, "False");
 const hp1 = pBook ("Harry Potter and the socerer's stone", "J.K. Rowling", 300, "True");
 const theGreatDivorce = pBook("The Great Divorce", "C.S. Lewis", 195, "True");
 let myLibrary = [hobbit, hp1, theGreatDivorce];
-const dispButton = document.querySelector('.addNewBook');
-const showBtn = document.querySelector('button');
 
 
 
-let formDisplay = document.getElementById("addNewBookForm");
-formDisplay.style.display = "none";
 
-dispButton.addEventListener('click', () => {expandCollapseForm()}
-);
 
-displayBooks();
+
+
 /*
 function book(title, author, pages, read) {
     this.title = title
@@ -49,104 +44,124 @@ function book(title, author, pages, read) {
     }
 }
 */
-function expandCollapseForm () {
+
+const library = (() => {
+    const dispButton = document.querySelector('.addNewBook');
+    const showBtn = document.querySelector('button');
+    let formDisplay = document.getElementById("addNewBookForm");
+    formDisplay.style.display = "none";
     
-    if (formDisplay.style.display === "none") {
-        formDisplay.style.display = "block";
-    } else {
-        formDisplay.style.display = "none";
-    }
-}
-
-function addBookToLibrary () { 
-   // preventDefault();
-    const newBookTitle = document.getElementById("bookTitle").value;
-    const newBookAuthor = document.getElementById("bookAuthor").value;
-    const newBookPages = document.getElementById("pageNumber").value;
-    const newBookRead = checkReadValue (); 
-     //alert(newBookTitle + newBookAuthor + newBookPages + newBookRead);
-    const newBook = pBook(newBookTitle, newBookAuthor, newBookPages, newBookRead);
-    myLibrary.push(newBook);
-    clearList();
-    displayBooks();
-    function checkReadValue () {
-        
-            const check = document.getElementsByName("read");
-            for (let i = 0; i < check.length; i++) {
-                if (check[i].checked) {
-                    return check[i].value;
-                }
-            }
-        
-    }
-}
-
-function displayBooks() {
-    clearList();
-    // I am making this loop to display all the contents of the array holding the objects that contain the Book data
-for (let i = 0; i < myLibrary.length; i++) {
-    const bookList = document.querySelector(".bookList");
-    const eachBook = document.createElement("div");
-    const removeButton = document.createElement("button")
-    const readOnOff = document.createElement("button")
-    const brk = document.createElement("br");
-    bookList.appendChild(eachBook);
-    bookList.appendChild(removeButton);
-    bookList.appendChild(brk);
-    bookList.appendChild(readOnOff);
-    removeButton.setAttribute("class", "removeButton");
-    removeButton.setAttribute('id',  i);
-    removeButton.dataset.rIndexNum = i; 
-    removeButton.addEventListener('click', (e) => {removeABook(e)} 
+    dispButton.addEventListener('click', () => {expandCollapseForm()}
     );
-    readOnOff.setAttribute("class", "readOnOffButton");
-    readOnOff.setAttribute("id", i);
-    readOnOff.dataset.inNum = i;
-    readOnOff.addEventListener('click',(e) => {
-        let dataNum = e.target.getAttribute("id");
-        myLibrary[dataNum].readToggle();
-        clearList();
-        displayBooks();
-    });
+    function expandCollapseForm () {
         
-        
-        //alert(dataNum);
-/*
-        myLibrary.forEach ()
-       for (let i = 0; i < myLibrary.length; i++){
-           if (dataNum == i) {
-               let pos = i;
-               myLibrary.splice(i, 1);
-               //alert(dataNum);
-           }
-       }
-        
-        */
-    
-    eachBook.setAttribute('class', "eachBook");
-    eachBook.setAttribute('id', myLibrary[i].title + i);
-    eachBook.innerHTML = myLibrary[i].bookInfo(); 
-    removeButton.innerHTML = "Remove Book";
-    readOnOff.innerHTML = "Toggle have or Haven't read";
-}
-}
-
-function clearList () {
-    const bookList = document.querySelector(".bookList");
-    while (bookList.firstChild) {
-        bookList.removeChild(bookList.firstChild);
+        if (formDisplay.style.display === "none") {
+            formDisplay.style.display = "block";
+        } else {
+            formDisplay.style.display = "none";
+        }
     }
-}
-
-function removeABook (e) {
-    let dataNum = e.target.getAttribute("id");
-    //alert(dataNum);
-    //alert(myLibrary);
-        myLibrary.splice(dataNum,1);
+    
+    function addBookToLibrary () { 
+       // preventDefault();
+        const newBookTitle = document.getElementById("bookTitle").value;
+        const newBookAuthor = document.getElementById("bookAuthor").value;
+        const newBookPages = document.getElementById("pageNumber").value;
+        const newBookRead = checkReadValue (); 
+         //alert(newBookTitle + newBookAuthor + newBookPages + newBookRead);
+        const newBook = pBook(newBookTitle, newBookAuthor, newBookPages, newBookRead);
+        myLibrary.push(newBook);
         clearList();
         displayBooks();
+        function checkReadValue () {
+            
+                const check = document.getElementsByName("read");
+                for (let i = 0; i < check.length; i++) {
+                    if (check[i].checked) {
+                        return check[i].value;
+                    }
+                }
+            
+        }
+    }
+    
+    function displayBooks() {
+        
+        clearList();
+        // I am making this loop to display all the contents of the array holding the objects that contain the Book data
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookList = document.querySelector(".bookList");
+        const eachBook = document.createElement("div");
+        const removeButton = document.createElement("button")
+        const readOnOff = document.createElement("button")
+        const brk = document.createElement("br");
+        bookList.appendChild(eachBook);
+        bookList.appendChild(removeButton);
+        bookList.appendChild(brk);
+        bookList.appendChild(readOnOff);
+        removeButton.setAttribute("class", "removeButton");
+        removeButton.setAttribute('id',  i);
+        removeButton.dataset.rIndexNum = i; 
+        removeButton.addEventListener('click', (e) => {removeABook(e)} 
+        );
+        readOnOff.setAttribute("class", "readOnOffButton");
+        readOnOff.setAttribute("id", i);
+        readOnOff.dataset.inNum = i;
+        readOnOff.addEventListener('click',(e) => {
+            let dataNum = e.target.getAttribute("id");
+            myLibrary[dataNum].readToggle();
+            clearList();
+            displayBooks();
+        });
+            
+            
+            //alert(dataNum);
+    /*
+            myLibrary.forEach ()
+           for (let i = 0; i < myLibrary.length; i++){
+               if (dataNum == i) {
+                   let pos = i;
+                   myLibrary.splice(i, 1);
+                   //alert(dataNum);
+               }
+           }
+            
+            */
+        
+        eachBook.setAttribute('class', "eachBook");
+        eachBook.setAttribute('id', myLibrary[i].title + i);
+        eachBook.innerHTML = myLibrary[i].bookInfo(); 
+        removeButton.innerHTML = "Remove Book";
+        readOnOff.innerHTML = "Toggle have or Haven't read";
+    }
+    }
+    
+    function clearList () {
+        const bookList = document.querySelector(".bookList");
+        while (bookList.firstChild) {
+            bookList.removeChild(bookList.firstChild);
+        }
+    }
+    
+    function removeABook (e) {
+        let dataNum = e.target.getAttribute("id");
+        //alert(dataNum);
+        //alert(myLibrary);
+            myLibrary.splice(dataNum,1);
+            clearList();
+            displayBooks();
+    }
+    return {
+        expandCollapseForm,
+        addBookToLibrary,
+        displayBooks,
+        clearList,
+        removeABook
+    };
 }
+)();
 
+library.displayBooks();
 
 /*
 function displayBooks() {
